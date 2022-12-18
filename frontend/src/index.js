aba153bd812cb8e51e1f2abd53ca9a8f2ebf6ebb
OuadieZerhouni <Ouadie.Zerhouni@usmba.ac.ma>
2022-12-18 22:05:40 +0100

    deleting simple form , adding google login

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,6 +6,7 @@ import reportWebVitals from './reportWebVitals';
 import RegisterForm from './Auth/RegisterForm';
 
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
