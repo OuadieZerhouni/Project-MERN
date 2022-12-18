1060bd34f3e29161dc44a53fc4fd67cd5ffce029
OuadieZerhouni <Ouadie.Zerhouni@usmba.ac.ma>
2022-12-18 15:24:27 +0100

    registeration complete

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -2,33 +2,41 @@ const express = require('express');
 const router = express.Router();
 const MongoClient = require('mongodb').MongoClient;
 
-// Replace <URI> with the connection string for your MongoDB database
-const uri = "MyDB";
+const uri = "mongodb://root1:0@localhost:27017/Project?retryWrites=true&w=majority";
 
 router.post('/register', (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
   // Connect to the database
-  MongoClient.connect(uri, { useNewUrlParser: true }, (err, client) => {
+  MongoClient.connect(uri, { useNewUrlParser: true }, async(err, client) => {
     if (err) {
       console.error(err);
       res.status(500).send(err);
       return;
     }
 
-    const collection = client.db("test").collection("users");
-    // Insert the new user into the "users" collection
+    const collection = client.db("Project").collection("users");
+
+    // Check if the email already exists in users(_id, email, password)
+    const emailExists = await collection.countDocuments({ email: email });
+    if (emailExists>0) {
+      res.status(400).send("Email already exists");
+      console.log("Email already exists "+ emailExists);
+      return;
+    }
+    else{
     collection.insertOne({ email: email, password: password }, (err, result) => {
       if (err) {
         console.error(err);
         res.status(500).send(err);
         return;
       }
-      res.send(result.ops[0]);
+      res.send("User created-----"+result.insertedId);  
+      console.log("User created-----"+result.insertedId);
       client.close();
-    });
-  });
+    })} ;
+  } );
 });
 
 module.exports = router;
