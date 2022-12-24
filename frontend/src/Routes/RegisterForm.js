64c5b3e930c0325b1d4ebc87c4f421a797101a76
OuadieZerhouni <Ouadie.Zerhouni@usmba.ac.ma>
2022-12-24 19:08:56 +0100

    commit 24/12

--- frontend/src/Routes/RegisterForm.js
+++ frontend/src/Routes/RegisterForm.js
@@ -1,38 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
-class RegisterForm extends React.Component {
-  state = {
-    email: '',
-    password: ''
-  }
-
-  handleChange = (event) => {
-    this.setState({ [event.target.name]: event.target.value });
-  }
-
-  handleSubmit = (event) => {
-    event.preventDefault();
-    const { email, password } = this.state;
-    axios.post('/auth/register', { email, password })
-      .then((response) => {
-        console.log(response.data);
-      })
-      .catch((error) => {
-        console.error(error);
-
-      });
-  }
-   
-  handleClick = (event) => {
-      axios.get('/auth/google')
-      .then((response) => {
-        console.log(response.data);
-      }
-      )
-    };
-  
-  render() {
+function RegisterForm (){
     return (
      <div>
         <a href="http://localhost:3000/auth/google"><button type="button"><img src="https://developers.google.com/identity/images/btn_google_signin_dark_normal_web.png" alt="Sign in " />
@@ -40,6 +9,6 @@ class RegisterForm extends React.Component {
       </div>
     );
   }
-}
+
 
 export default RegisterForm;
