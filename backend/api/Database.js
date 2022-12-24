845a6af35636b748805f4fb2ee1c723181dcf53e
OuadieZerhouni <Ouadie.Zerhouni@usmba.ac.ma>
2022-12-24 19:09:06 +0100

    .

---
+++ backend/api/Database.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const MongoClient = require('mongodb').MongoClient;
+
+
+const app = express();
+
+
+const MONGODB_URI = 'mongodb://root1:0@127.0.0.1:27017/Project?retryWrites=true&w=majority';
+const MONGODB_DB_NAME = 'Project';
+
+app.get('/users', (req, res) => {
+    MongoClient.connect(MONGODB_URI, { useNewUrlParser: true })
+        .then(client => {
+            const db = client.db(MONGODB_DB_NAME);
+            const usersCollection = db.collection('users');
+            return usersCollection.find().toArray();
+        })
+        .then(users => res.json(users))
+        .catch(error => console.error(error));
+}
+);
+
+module.exports = app;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 
 const express = require('express');
 const googleAuth = require('./routes/google');
+const database = require('./api/database');
 
 const app = express();
 app.use('/auth', googleAuth);
+app.use('/api/database', database)
 app.listen(3000, () => console.log('Server listening on port 3000'));
